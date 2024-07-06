b0e295c363116ad79b39df2dac5e08f7acc65aff
Alex Rodrigues <alex.rodrigues@upside.com>
2024-07-06 20:43:04 -0300

    fixing settings

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -19,8 +20,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Confirm Sign Out"),
-          content: Text("Are you sure you want to sign out?"),
+          title: Text("Confirmação"),
+          content: Text("Tem certeza que deseja sair?"),
           actions: [
             TextButton(
               child: Text("Cancel"),
@@ -29,7 +30,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               },
             ),
             TextButton(
-              child: Text("Sign Out"),
+              child: Text("Sair"),
               onPressed: () {
                 Navigator.of(context).pop();
                 _signOut();
@@ -41,12 +42,50 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 
+  Future<void> _reportBug() async {
+    final Uri emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'alex.rodrigues.developer@gmail.com',
+      queryParameters: {
+        'subject': 'Bug Report',
+        'body': 'Por favor descreva o bug encontrado'
+      },
+    );
+
+    try {
+      await launch(emailLaunchUri.toString());
+    } catch (e) {
+      // Handle error
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SettingsList(
       sections: [
         SettingsSection(
-          title: Text('Conta'),
+          title: const Text(
+            'Contato',
+            style: TextStyle(
+              color: Colors.black, // Change the color to blue
+            ),
+          ),
+          tiles: <SettingsTile>[
+            SettingsTile(
+              title: Text("Reportar um bug"),
+              onPressed: (context) {
+                _reportBug();
+              },
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: const Text(
+            'Conta',
+            style: TextStyle(
+              color: Colors.black, // Change the color to blue
+            ),
+          ),
           tiles: <SettingsTile>[
             SettingsTile(
               title: Text("Sair"),
